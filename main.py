76cc2bfe9b83c43858b9add46026244bbd0b4e10
Viviank <vivianne35kerubo@gmail.com>
2021-04-14 11:20:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 import classifiers 
 from classifiers import inception
 from classifiers import nne
-#from classifiers.inception import InceptionTime
-#from classifiers.nne import NNE
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, TensorDataset
@@ -33,7 +31,8 @@ from torch.utils.data import DataLoader, TensorDataset
 
 device = torch.device("cpu")
 
-def prepare_data():
+def prepare_data(datasets_dict, dataset_name):
+    
     x_train = datasets_dict[dataset_name][0]
     y_train = datasets_dict[dataset_name][1]
     x_test = datasets_dict[dataset_name][2]
@@ -67,9 +66,16 @@ def prepare_data():
     y_train = torch.from_numpy(y_train)
     y_test = torch.from_numpy(y_test)
     y_true = torch.from_numpy(y_true)
+    
+    
+    train_dataset = torch.utils.data.TensorDataset(x_train, y_train)
+    test_dataset = torch.utils.data.TensorDataset(x_test, y_true)
+    train_dataloader = DataLoader(train_dataset, batch_size=int(min(x_train.shape[0] / 10, 16)), shuffle=True, num_workers= 0)
+    test_dataloader = DataLoader(test_dataset, batch_size=int(min(x_train.shape[0] / 10, 16)), shuffle=False, num_workers= 0)
    
+    return train_dataloader, test_dataloader, nb_classes, y_true, enc
 
-    return x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc
+    #return x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc
 
 use_cuda = torch.cuda.is_available()
 def mixup_data(x, y, alpha=0.4, use_cuda=False):
@@ -90,7 +96,7 @@ def mixup_data(x, y, alpha=0.4, use_cuda=False):
     
     return mixed_x, mixed_y         
 
-def train(model, optimizer, criterion, x_train, y_train,x_test,y_test, device):
+def train(model, optimizer, criterion, dataloader, device):
 
         
         model.train()
@@ -98,18 +104,39 @@ def train(model, optimizer, criterion, x_train, y_train,x_test,y_test, device):
         start_time = time.time()
         optimizer.zero_grad()
         #x, y = mixup_data(x_train, y_train, 0.4,use_cuda)
-        x, y = x_train,y_train
-        x = x.float()
-        x, y = x.to(device), y.to(device)
-        output = model.forward(x)
-        loss =  criterion(output, torch.max(y,1)[1])
-        loss.backward()
-        optimizer.step()
-        losses.append(loss)
+        with tqdm(enumerate(dataloader), total=len(dataloader), leave=True) as iterator:
+          for idx, batch in iterator:
+            x, y = batch
+            x = x.float()
+            x, y = x.to(device), y.to(device)
+            output = model.forward(x)
+            loss =  criterion(output, torch.max(y,1)[1])
+            loss.backward()
+            optimizer.step()
+            losses.append(loss)
         return torch.stack(losses)
 
+def train_mixup(model, optimizer, criterion, dataloader, device):
 
-def test(model,criterion, x_test, y_true, x_train, y_train, y_test,device):
+        
+        model.train()
+        losses = list()
+        start_time = time.time()
+        optimizer.zero_grad()
+        with tqdm(enumerate(dataloader), total=len(dataloader), leave=True) as iterator:
+          for idx, batch in iterator:
+            x, y = batch
+            x, y = mixup_data(x, y, alpha=0.4)
+            x = x.float()
+            x, y = x.to(device), y.to(device)
+            output = model.forward(x)
+            loss =  criterion(output, torch.max(y,1)[1])
+            loss.backward()
+            optimizer.step()
+            losses.append(loss)
+        return torch.stack(losses)
+    
+def test(model,criterion, dataloader,device):
       
       model.eval()
       with torch.no_grad():
@@ -118,16 +145,20 @@ def test(model,criterion, x_test, y_true, x_train, y_train, y_test,device):
         y_pred_list = list()
         y_score_list = list()
         start_time = time.time()
-        x_test = x_test.float()
-        y_pred = model.forward(x_test.to(device))
-        loss = criterion(y_pred, y_true.to(device))
-        losses.append(loss)
-        y_true_list.append(y_true)
-        y_pred_list.append(y_pred.argmax(-1))
-        y_score_list.append(y_pred.exp())
+        with tqdm(enumerate(dataloader), total=len(dataloader), leave=True) as iterator:
+            for idx, batch in iterator:
+              x_test, y_true = batch
+              x_test = x_test.float()
+              y_pred = model.forward(x_test.to(device))
+              loss = criterion(y_pred, y_true.to(device))
+              losses.append(loss)
+              y_true_list.append(y_true)
+              y_pred_list.append(y_pred.argmax(-1))
+              y_score_list.append(y_pred.exp())
 
         return torch.stack(losses), torch.cat(y_true_list), torch.cat(y_pred_list), torch.cat(y_score_list)
-#function to save the best model check point
+
+ #function to save the best model check point
 def save_ckp(state, is_best, checkpoint_path, best_model_path):
     """
     state: checkpoint we want to save
@@ -144,7 +175,7 @@ def save_ckp(state, is_best, checkpoint_path, best_model_path):
         # copy that checkpoint file to best path given, best_model_path
         shutil.copyfile(f_path, best_fpath)
 
-def training(logdir,model,test_loss_min_input,checkpoint_path, best_model_path):
+def training(logdir,model,test_loss_min_input,checkpoint_path, best_model_path,train_dataloader, test_dataloader, apply_mixup):
   global test_loss_min
   device = torch.device("cpu")
   criterion = torch.nn.CrossEntropyLoss(reduction="mean")
@@ -163,9 +194,13 @@ def training(logdir,model,test_loss_min_input,checkpoint_path, best_model_path):
   test_loss_min = test_loss_min_input
   for epoch in range(epochs):
 
-    train_loss = train(model, optimizer, criterion,x_train, y_train, x_test,y_test,device)
+    #train_loss = train(model, optimizer, criterion,x_train, y_train, x_test,y_test,device)
+    if apply_mixup:
+      train_loss = train_mixup(model, optimizer, criterion,train_dataloader,device)
+    else:
+      train_loss = train(model, optimizer, criterion,train_dataloader,device)
     test_loss, y_true_, y_pred, _ = test(model,criterion, x_test, y_true, x_train, y_train, y_test,device)
-    scores = metrics(y_true.cpu(), y_pred.cpu())
+    scores = metrics(y_true_.cpu(), y_pred.cpu())
     scores_msg = ", ".join([f"{k}={v:.2f}" for (k, v) in scores.items()])
     test_loss = test_loss.cpu().detach().numpy()[0]
     train_loss = train_loss.cpu().detach().numpy()[0]
@@ -214,15 +249,12 @@ def get_model(modelname, num_classes, input_dim, num_layers, hidden_dims, device
 
 
 ############################################### main
- 
-root_dir = '/content/gdrive/MyDrive/Inception_time/InceptionTime/archives/UCR_TS_Archive_2015'
-xps = ['use_bottleneck', 'use_residual', 'nb_filters', 'depth',
+def experiment(use_mixup, train_mixup):
+    root_dir = '/content/gdrive/MyDrive/Inception_time/InceptionTime/archives/UCR_TS_Archive_2015'
+    xps = ['use_bottleneck', 'use_residual', 'nb_filters', 'depth',
        'kernel_size', 'batch_size']
 
-print(len(sys.argv))
-import sys
-
-if sys.argv[1] == 'InceptionTime':
+    #if sys.argv[1] == 'InceptionTime':
     # run nb_iter_ iterations of Inception on the whole TSC archive  
     classifier_name = 'inception'
     archive_name = ARCHIVE_NAMES[0]
@@ -239,12 +271,18 @@ if sys.argv[1] == 'InceptionTime':
         if iter != 0:
             trr = '_itr_' + str(iter)
 
-        tmp_output_directory = root_dir + '/results1-test-m/' + classifier_name + '/' + archive_name + trr + '/'
+        
+        #tmp_output_directory = root_dir + '/results1-test-m/' + classifier_name + '/' + archive_name + trr + '/'
+        if use_mixup:
+          tmp_output_directory = root_dir + '/results-test-mixup/' + classifier_name + '/' + archive_name + trr + '/'
+        else:
+          tmp_output_directory = root_dir + '/results-test/' + classifier_name + '/' + archive_name + trr + '/'
 
         for dataset_name in utils.constants.dataset_names_for_archive[archive_name]:
             print('\t\t\tdataset_name: ', dataset_name)
 
-            x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc = prepare_data()
+            #x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc = prepare_data()
+            train_dataloader, test_dataloader, nb_classes, y_true, enc = prepare_data(datasets_dict, dataset_name)
             
 
             output_directory = tmp_output_directory + dataset_name + '/'
@@ -258,8 +296,20 @@ if sys.argv[1] == 'InceptionTime':
             
             checkpoint_path = temp_output_directory + "/" + "current_checkpoint.pt"
             best_model_path = temp_output_directory + "/" + "best_model.pt"
-            model = get_model(modelname = classifier_name,num_classes=nb_classes,input_dim=1, num_layers=6, hidden_dims=32, device = device)
-            training(temp_output_directory, model, np.inf,checkpoint_path, best_model_path)
+            model = get_model(modelname = classifier_name,num_classes=nb_classes,input_dim=1, num_layers=6, hidden_dims= 128, device = device)
+            #training(temp_output_directory, model, np.inf,checkpoint_path, best_model_path)
+            if train_mixup:
+              training(temp_output_directory, model, np.inf,checkpoint_path, best_model_path,train_dataloader,
+              test_dataloader,True)
+            else:
+              training(temp_output_directory, model, np.inf,checkpoint_path, best_model_path,train_dataloader,
+              test_dataloader, False)
+           
+        
+        
+if __name__ == "__main__":
+
+    experiment(False, False) # train without mixup set as False, train with mixup set as True 
            
               
         ############### run the ensembling of these iterations of Inception ################### 
